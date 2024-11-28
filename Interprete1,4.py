cb5c02456572faca5ce4574a225b2f5a5174f9c8
Catto <106460130+Catto2004@users.noreply.github.com>
2024-11-28 15:41:45 -0500

    Se removieron: OutEntrada.py, prueba.py, and tree.dot Y se cambió a la versión 1,4 del Interprete.

--- Interprete1,4.py
+++ Interprete1,4.py
@@ -3,10 +3,11 @@ import sys
 import os
 
 # Cambiar a elección.
-sinarboles = False  # Si se desea graficar el árbol sintáctico
-arboldot = True  # Si se desea guardar el árbol sintáctico como archivo DOT
-arbolpng = True  # Si se desea guardar el árbol sintáctico como imagen PNG
-arbolcmd = False  # Si se desea mostrar el árbol sintáctico en la consola
+class Config:
+    SinArboles = False  # Si se desea graficar el árbol sintáctico
+    Arboldot = True  # Si se desea guardar el árbol sintáctico como archivo DOT
+    Arbolpng = True  # Si se desea guardar el árbol sintáctico como imagen PNG
+    Arbolcmd = False  # Si se desea mostrar el árbol sintáctico
 
 # To-Do:
 # Ya implementado:
@@ -14,34 +15,36 @@ arbolcmd = False  # Si se desea mostrar el árbol sintáctico en la consola
 #                | sugar
 #                | math
 #                | Variables
-#                > Arboles PNG y DOT
-# Aún no implementado:
+#                | function
+#                | call
 #                | bool
 #                | control
 #                | loop
-#            x   | function, Demasiado complicado, preguntar???
-#            x   | call, Demasiado complicado
+#                > Arboles PNG y DOT
+# Aún no implementado:
+
 
 
 # ############################## Mensajes ##############################
 # Creditos
-Creditos = """
-#          ^~^  ,      This code was generated using the Logo++ Interpreter
-#         ('Y') )      by [Catto] Juan Diego Ruiz B, Juan Camilo Marin H. && Luisa Lopez.
-#         /   \/       special thanks to: Mochi.
-#        (\|||/)       >> This code is under MIT License. <<
-"""
-# Mensaje de bienvenida
-MensajeInterprete = """
+class Mensajes:
+    Creditos = """
+          ^~^  ,      This code was generated using the Logo++ Interpreter
+         ('Y') )      by [Catto] Juan Diego Ruiz B, Juan Camilo Marin H. && Luisa Lopez.
+         /   \/       special thanks to: Mochi.
+        (\|||/)       >> This code is under MIT License. <<
+    """
+    # Mensaje de bienvenida
+    MensajeInterprete = """
            \033[37;1m Logo++ Interpreter \033[0m
-"""
-# Gato para mensaje
-Gato = """0======v================================0
-       \ \033[93m  ^~^  ,
-          ('Y') )
-          /   \/
-         (\|||/)
-\033[0m"""
+    """
+    # Gato para mensaje
+    Gato = """0======v================================0
+           \ \033[93m  ^~^  ,
+              ('Y') )
+              /   \/
+             (\|||/)
+    \033[0m"""
 
 
 # ############################## Gramatica ##############################
@@ -51,6 +54,8 @@ LogoPP = r"""
     ?instruction: basic
                 | VARIABLE "=" math -> assign
                 | sugar
+                | function
+                | call
 
     ?basic:       "FD" math -> fd
                 | "BK" math -> bk
@@ -74,14 +79,34 @@ LogoPP = r"""
                 | factor
 
     ?factor:      INTNUM
-                | VARIABLE -> var
-                | "-" factor -> neg
+                | VARIABLE       -> var
+                | "-" factor     -> neg
                 | "(" math ")"
 
+    ?bool:        atom
+                | bool "AND" bool  -> andOper
+                | bool "OR" bool   -> orOper
+                | "NOT" bool       -> notOper
+
+    ?atom:        "TRUE"           -> trueVal
+                | "FALSE"          -> falseVal
+
+    ?function:    "DEF" VARIABLE "(" [VARIABLE ("," VARIABLE)*] ")" "{" instruction+ "}" -> defFunction
+
+    ?call:        VARIABLE "(" [VARIABLE ("," VARIABLE)*] ")"                            -> callFunction
+
+    ?control:     "IF" bool "{" instruction+ "}" -> ifOper
+
+    ?loop:        "FOR" VARIABLE "IN" "RANGE(" VARIABLE ")" "{" instruction+ "}"                      -> forOper
+                | "FOR" VARIABLE "," VARIABLE "IN" "ZIP(" "RANGE(" INTNUM "," INTNUM "," INTNUM ")" "," "RANGE(" INTNUM "," INTNUM "," INTNUM ")" ")" "{" instruction+ "}" -> forforOper
+
+    UNEXPECTED: /.+/
+
+    %ignore UNEXPECTED
     %ignore /#[^\n]*/
-    VARIABLE: /[a-zA-Z_][a-zA-Z0-9_]*/
-    INTNUM: /-?\d+(\.\d+)?([eE][+-]?\d+)?/x
-    %ignore /[ \t\n\f\r]+/x
+    VARIABLE: /[a-zA-Z_][a-zA-Z0-9_]*/  
+    INTNUM: /-?\d+(\.\d+)?([eE][+-]?\d+)?/x  
+    %ignore /[ \t\n\f\r]+/x 
 """
 
 
@@ -173,29 +198,75 @@ class CalcularArbol(Transformer):
         return f"(-{args[0]})"
     def INTNUM(self, value):
         return int(value)
+    
+    ############### Definición y llamada de funciones
+    def defFunction(self, args):
+        function_name = str(args[0])
+        parameters = [str(arg) for arg in args[1:]]
+        self.functions[function_name] = parameters
+        param_list = ", ".join(parameters)
+        return f"def {function_name}({param_list}):"
 
+    def callFunction(self, args):
+        function_name = str(args[0])
+        arguments = [str(arg) for arg in args[1:]]
+        if function_name in self.functions:
+            arg_list = ", ".join(arguments)
+            return f"{function_name}({arg_list})"
+        else:
+            raise ValueError(f"Función no definida: {function_name}")
+
+    ############### Operaciones booleanas
+    def trueVal(self, _):
+        return True
+    def falseVal(self, _):
+        return False
+    def andOper(self, args):
+        return f"({args[0]} and {args[1]})"
+    def orOper(self, args):
+        return f"({args[0]} or {args[1]})"
+    def notOper(self, args):
+        return f"(not {args[0]})"
+
+    ############### Control
+    def ifOper(self, args):
+        return f"if {args[0]}:"
+    def elseOper(self, _):
+        return "else:"
+    def elifOper(self, args):
+        return f"elif {args[0]}:"
+
+    ############### Ciclos
+    def forOper(self, args):
+        return f"for {args[0]} in range({args[1]}):"
+    
+    def forforOper(self, args):
+        return f"for {args[0]}, {args[1]} in zip(range({args[2]}, {args[3]}, {args[4]}), range({args[5]}, {args[6]}, {args[7]}):"
+    
 
 # ############################## Procesamiento de Archivos ##############################
 # Graficador de arboles
 def procesar_ast(arbol):
-    if not sinarboles:
+    if not Config.SinArboles:
         try:
             # Imprimir el AST en la consola
-            if arbolcmd:
+            if Config.Arbolcmd:
                 print("\033[1;34mÁrbol Sintáctico:\033[0m")
                 print(arbol.pretty())
 
             # Guardar el AST como imagen y archivo DOT
-            if arbolpng:
+            if Config.Arbolpng:
                 tree.pydot__tree_to_png(arbol, "tree.png")  # Guarda como imagen PNG
             else:
                 print("\033[1;33mEl árbol sintáctico en PNG no se mostrará.\033[0m")
-            if arboldot:
+
+            if Config.Arboldot:
                 tree.pydot__tree_to_dot(arbol, "tree.dot", rankdir="TD")  # Guarda como archivo DOT
             else:
                 print("\033[1;33mEl árbol sintáctico en DOT no se creará.\033[0m")
-            if arbolpng or arboldot:
-                print("\033[1;32mAST visual guardado como 'tree.png' y 'tree.dot'.\033[0m")
+
+            if Config.Arbolpng or Config.Arboldot:
+                print("\033[1;32mAST visual guardado como 'tree.png' y/o 'tree.dot'.\033[0m")
             else:
                 print("\033[1;33mRevisar la configuración\033[0")
 
@@ -233,7 +304,7 @@ def convertir_archivo(input_file, output_file):
 
             # Finalizar con el mainloop de turtle
             outfile.write("\nturtle.mainloop()\n")
-            outfile.write("\n" + Creditos)
+            outfile.write("\n" + Mensajes.Creditos)
 
         print(f"\033[1;37mArchivo convertido y guardado en: \033[35m{output_file}\033[0m")
     except Exception as e:
@@ -243,23 +314,28 @@ def convertir_archivo(input_file, output_file):
 # ############################## Ejecución del programa ##############################
 # Mostrar mensaje de bienvenida
 os.system("cls" if os.name == "nt" else "clear")
-print(MensajeInterprete)
+print(Mensajes.MensajeInterprete)
 
 # Crear parser con transformador
-parser = Lark(LogoPP, parser="lalr")
+try:
+    parser = Lark(LogoPP, parser="lalr")
+    print("Gramática cargada correctamente.")
+except Exception as e:
+    print(f"Error al cargar la gramática: {e}")
+
 
 # Leer los argumentos de la línea de comandos
 if len(sys.argv) != 2:
     print("0=======================================0")
     print("\033[1;31m               Be careful!\033[0m")
     print(" Usage: python interprete.py archivo.lpp")
-    print(Gato)
+    print(Mensajes.Gato)
 else:
     input_file = sys.argv[1]
     output_file = "Out" + os.path.basename(input_file).replace('.lpp', '.py')
 
     print("0=======================================0")
     print("            \033[1;37m You can do this!\033[0m")
-    print(Gato)
+    print(Mensajes.Gato)
     # Convertir el archivo
     convertir_archivo(input_file, output_file)
